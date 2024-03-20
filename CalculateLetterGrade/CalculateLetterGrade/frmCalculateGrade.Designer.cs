c976873e3deab1610aa798d2248a703b05d915d0
Lorenzo <122297489+LWNVII@users.noreply.github.com>
2024-03-20 15:42:15 -0400

    Add files via upload

---
+++ CalculateLetterGrade/CalculateLetterGrade/frmCalculateGrade.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CalculateLetterGrade
+{
+    partial class frmCalculateGrade
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtNumberGrade = new TextBox();
+            txtLetterGrade = new TextBox();
+            btnCalculate = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 15);
+            label1.TabIndex = 0;
+            label1.Text = "&Number Grade";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(49, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Letter Grade";
+            // 
+            // txtNumberGrade
+            // 
+            txtNumberGrade.Location = new Point(160, 33);
+            txtNumberGrade.Name = "txtNumberGrade";
+            txtNumberGrade.Size = new Size(68, 23);
+            txtNumberGrade.TabIndex = 1;
+            // 
+            // txtLetterGrade
+            // 
+            txtLetterGrade.Location = new Point(160, 73);
+            txtLetterGrade.Name = "txtLetterGrade";
+            txtLetterGrade.ReadOnly = true;
+            txtLetterGrade.Size = new Size(68, 23);
+            txtLetterGrade.TabIndex = 3;
+            txtLetterGrade.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(49, 117);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(75, 23);
+            btnCalculate.TabIndex = 2;
+            btnCalculate.Text = "&Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(153, 117);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // frmCalculateGrade
+            // 
+            AcceptButton = btnCalculate;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnExit;
+            ClientSize = new Size(304, 168);
+            Controls.Add(btnExit);
+            Controls.Add(btnCalculate);
+            Controls.Add(txtLetterGrade);
+            Controls.Add(txtNumberGrade);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmCalculateGrade";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculate Letter Grade";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtNumberGrade;
+        private TextBox txtLetterGrade;
+        private Button btnCalculate;
+        private Button btnExit;
+    }
+}
---
+++ ChangeCalculator/ChangeCalculator/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeCalculator
+{
+    public partial class ChangeCalculator : Form
+    {
+        
+
+        public ChangeCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input = Convert.ToInt32(txtInput.Text);
+            int quarters = input / 25;
+            int pennies = input % 25;
+            txtQuarters.Text = quarters.ToString();
+
+            int dimes = pennies / 10;
+            pennies = pennies % 10;
+            txtDimes.Text = dimes.ToString();
+
+            int nickels = pennies / 5;
+            pennies = pennies % 5;
+            txtNickels.Text = nickels.ToString();
+
+            txtPennies.Text = pennies.ToString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
