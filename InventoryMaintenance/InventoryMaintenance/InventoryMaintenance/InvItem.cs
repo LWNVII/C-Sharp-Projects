899fdbe6757f91a91289120d5c236835a3ca1e1d
Lorenzo <122297489+LWNVII@users.noreply.github.com>
2024-03-20 15:47:19 -0400

    Add files via upload

---
+++ InventoryMaintenance/InventoryMaintenance/InventoryMaintenance/InvItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class InvItem
+    {
+        public int ItemNo { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public InvItem() 
+        {
+            ItemNo = 0;
+            Description = "";
+            Price = 0.0m;
+        }
+
+        public InvItem(int itemNo, string description, decimal price)
+        {
+            ItemNo = itemNo;
+            Description = description;
+            Price = price;
+        }
+
+        public string GetDisplayText()
+        {
+            return $"{ItemNo}    {Description} (${Price:F2})";
+        }
+    }
+}
